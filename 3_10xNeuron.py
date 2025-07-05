92616a3640f1619890bcdf6908c65f9baa0f93ca
YRyaaan <yiyi0805@163.com>
2025-07-05 19:35:06 +0800

    Add files via upload

---
+++ 3_10xNeuron.py
@@ -0,0 +1,26 @@
+import anndata
+import pandas as pd
+import scanpy as sc
+
+data = anndata.read_h5ad("D:/GRADE3/RESEARCH_singlecell/data整合/data/3_10xNeuron/paired_full_features.h5ad")
+
+#scRNA-seq表达情况矩阵
+scRNA_matrix = data.X
+print(scRNA_matrix)
+
+#scRNA-seq dim的行列名
+scRNA_row = data.obs_names.tolist()
+print(scRNA_row)
+scRNA_col = data.var_names.tolist()
+print(scRNA_col)
+
+atac_features = data.var_names[data.var['modality'] == 'Peaks']
+#scATAC-seq peaks表达情况矩阵
+atac_matrix = data[:, atac_features].X
+print(atac_matrix)
+#scATAC-seq dim的行列名
+scATAC_row = data[:, atac_features].obs_names.tolist()
+print(scATAC_row)
+scATAC_col = data[:, atac_features].var_names.tolist()
+print(scATAC_col)
+
